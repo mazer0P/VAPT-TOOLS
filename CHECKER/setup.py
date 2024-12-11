71976bc15ef12636c5445db698491dd0dc3dab7e
Paras Bhardwaj <75685411+mazer0P@users.noreply.github.com>
2024-12-11 15:54:17 +0530

    Update setup.py
    updated setup information

--- CHECKER/setup.py
+++ CHECKER/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='checker',                # Name of the package
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[              # Dependencies
         'requests',
